a8dbdbaddbe31fbb051448f2473f886bd87056a5
Ra8t6y1 <Ramachandrarao_Tatavarty@gap.com>
2017-08-23 01:51:10 -0700

    adding resources folder changes

--- driver/src/main/java/com/omni/module/driver/DriverInit.java
+++ driver/src/main/java/com/omni/module/driver/DriverInit.java
@@ -3,6 +3,7 @@ package com.omni.module.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,12 +16,14 @@ public class DriverInit {
 
     public WebDriver getDriver(String browserType) {
 
-        driver = new FirefoxDriver();
+        //gecko driver need to be worked on in Mac
+        /*driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        System.setProperty("webdriver.firefox.marionette", "path/geckodriver");*/
 
         if(browserType.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.omnitest", "/Users/rtatavarty/chromedriver/chromedriver");
+            System.setProperty("webdriver.chrome.omnitest", "chromedriver");
             driver = new ChromeDriver();
         }
 
@@ -30,8 +33,7 @@ public class DriverInit {
         }
 
         else if(browserType.equalsIgnoreCase("Safari")) {
-            System.setProperty("webdriver.chrome.omnitest", "/Users/rtatavarty/chromedriver/chromedriver");
-            driver = new ChromeDriver();
+            driver = new SafariDriver();
         }
 
         return driver;
